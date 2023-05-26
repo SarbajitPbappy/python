0b6e6ec14b8d6b700bce0d77dbf85f613eb3b549
Sarbajit Paul <you@example.com>
2023-05-26 18:29:36 +0600

    assingment 1

--- Exam 1/problem4.py
+++ Exam 1/problem4.py
@@ -1,13 +1,23 @@
-# args:
-def SUM(*args):
-    sum = 0
-    for i in args:
-        sum += i
-    return sum
-print(SUM(1,2,3,4,5,6,7,8,9,10))
+# Minimize Number
+flag=True
+counter=0
+size=int(input())
+arr=[]
+ele=input().split()
+arr=[int(num) for num in ele]
+for num in arr:
+    if num % 2 != 0:
+        flag = False
+        break
+while flag:
+    for num in arr:
+        if num % 2 != 0:
+            flag = False
+            break
+        arr[arr.index(num)] = num / 2
+    if flag:
+        counter += 1
+    else:
+        break
+print(counter)
 
-# kwargs:
-def KWARGS(**kwargs):
-    for key,value in kwargs.items():
-        print(f"{key} = {value}")
-KWARGS(name='Sakib',age=23,roll=123,dist='Dhaka',country='Bangladesh',phone=1234567890,profession='Student')
